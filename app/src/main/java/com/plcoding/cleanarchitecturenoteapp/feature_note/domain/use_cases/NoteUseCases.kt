c48d19cc8ce1bec8f89a07940345c0a7d9828f33
PhilNikS <philip1997ctr@gmail.com>
2023-07-29 21:33:18 +0300

    The first initialize Half of project

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_cases/NoteUseCases.kt
@@ -0,0 +1,7 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_cases
+
+data class NoteUseCases(
+    val getNotes: GetNotesUseCase,
+    val deleteNote: DeleteNoteUseCase,
+    val addNote: AddNoteUseCase
+)
